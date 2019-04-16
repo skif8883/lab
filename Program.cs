7acc2c1556a188314a0b80521cf67e47fea3cc44
skif8883 <46078950+skif8883@users.noreply.github.com>
2019-04-16 11:45:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a * b;
+            d = c * c;
+            Console.WriteLine(d);
+
+        }
+    }
+}
